08e1dace075eba8dbbb2ee159e168e530cdfd723
Mr. Twinkle Sharma <smtwinkle451@gmail.com>
2023-01-25 19:11:26 +0530

    Optional Chaining

--- src/Components/UserBadgeItem.jsx
+++ src/Components/UserBadgeItem.jsx
@@ -4,9 +4,9 @@ const UserBadgeItem = ({ user, handleFunction, admin, currentUser }) => {
   // console.log(user, admin);
   return (
     <div className="p-2 rounded-md inline bg-[#11256D] text-white">
-      {user.name}
-      {currentUser._id === user._id && <span> (You)</span>}
-      {admin._id === user._id && <span> (Admin)</span>}
+      {user?.name}
+      {currentUser?._id === user?._id && <span> (You)</span>}
+      {admin?._id === user?._id && <span> (Admin)</span>}
       <MdOutlinePersonRemoveAlt1 size={22}
         className='inline my-auto mx-2 cursor-pointer'
         onClick={handleFunction} />
